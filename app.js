4c5f2ba9505c6b43a5a85daae55642ef8aaa5941
Dru Riley <drurly@gmail.com>
2014-02-12 06:28:53 -0500

    Add 1 year request header for cache control

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-var express = require('express'),
-    http    = require('http'),
-    request = require('request'),
-    app     = express();
+var express      = require('express'),
+    http         = require('http'),
+    request      = require('request'),
+    cacheControl = require('express-cache-control'),
+    app          = express(),
+    cache        = new cacheControl().middleware;
 
 app.use(express.bodyParser());
 
-app.get('/:bucketName/:imageName', function(req, res){
+app.get('/:bucketName/:imageName', cache("year", 1), function(req, res){
   var bucketName = req.params.bucketName,
       imageName  = req.params.imageName;
   request('http://s3.amazonaws.com/' + bucketName + '/' + imageName).pipe(res)
